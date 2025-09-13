33d7c7139dc69f1461ce45b9b5ea57f38656522e
ahmedta <a.alrakhawy@gmail.com>
2025-09-14 01:32:14 +0300

    chore: connect mongodb
    edit .env.example to .env and added monogdb connection string
    
    also fixed lint error in environtments.ts and onclick error in sidebar.ts

--- server/src/config/environtments.ts
+++ server/src/config/environtments.ts
@@ -1,3 +1,8 @@
+/*
+dotenv is a small Node.js library.
+It helps you load secret configuration values (like database passwords, API keys)
+from a file called .env into process.env.
+ */
 import "dotenv/config";
 
 export const config = {
